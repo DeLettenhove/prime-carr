ec66a89f4cde676b82285a05145e4e3c02ab6e11
DeLettenhove <deletten@gmail.com>
2022-11-03 14:57:17 +0200

    Add hover effect for card component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Cars from './pages/cars/Cars'
 
 function App() {
   return (
-    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cars" element={<Cars />} />
         <Route path="/cars/:id" element={<h1>Chosen car!!!</h1>} />
         <Route path="*" element={<h1>Not Found!!!</h1>} />
       </Routes>
-    </>
   )
 }
 
